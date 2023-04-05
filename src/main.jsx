37ddb9b45d966d290ba71320a98d0735eb202d1d
ashraful amin <ashrafulamin654@gmail.com>
2023-04-05 15:02:22 +0600

    54-3 Avoid reload using Link and Load data on route

---
+++ src/main.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './Component/About/About';
+import Contact from './Component/Contact/Contact';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import First from './Component/First/First';
+import Friends from './Component/Friends/Friends';
+// import ErrorPage from "./error-page";
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <App></App>
+//   },
+//   {
+//     path:'/about',
+//     element: <About></About>
+//   },
+//   {
+//     path:'/contact',
+//     element: <Contact></Contact>
+//   },
+
+
+// {
+//   path: "/",
+//   element: <Root />,
+//   errorElement: <ErrorPage />,
+// },
+// ])
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <First></First>
+      },
+      {
+        path: '/friends',
+        element: <Friends></Friends>,
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
